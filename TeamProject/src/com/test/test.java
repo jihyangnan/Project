c728ad9914734b8c4345d47cbb00d40d941abac5
hyangnan <wlgidsks0913@naver.com>
2015-10-21 11:57:53 +0900

    aaa

--- TeamProject/src/com/test/test.java
+++ TeamProject/src/com/test/test.java
@@ -4,9 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("수정");
-		System.out.println("수정22");
-		System.out.println("수정3");
-		System.out.println("수정4");
+		System.out.println("수정2");
 	}
 
 }
