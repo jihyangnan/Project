4da29bb76fcb0cf74eb66f0b0552247ed841dafc
hyangnan <wlgidsks0913@naver.com>
2015-10-28 09:58:53 +0900

    board

---
+++ TeamProject/src/com/board/dao/BoardDTO.java
@@ -0,0 +1,78 @@
+package com.board.dao;
+import java.util.*;
+/*
+ -- 게시판 테이블
+  CREATE TABLE p_board(
+  b_no NUMBER PRIMARY KEY NOT NULL, --글번호(pk)
+  b_Id VARCHAR2(20) NOT NULL, -- 작성자(fk)
+  b_Title VARCHAR2(50) NOT NULL, -- 제목
+  b_Content VARCHAR2(2000) NOT NULL, -- 내용
+  b_Hit NUMBER NOT NULL, -- 조회수
+  b_Renum NUMBER, -- 답변수
+  b_Date DATE NOT NULL, --등록일
+  b_Delete CHAR(2) NOT NULL, --삭제여부
+  b_saNo NUMBER REFERENCES b_Word(sa_No) -- 말머리 번호 (fk)
+);
+
+ */
+public class BoardDTO {
+	private int b_no;
+	private String b_id;
+	private String b_title;
+	private String b_content;
+	private int b_hit;
+	private int b_renum;
+	private Date b_date;
+	private String b_delete;
+	
+	public int getB_no() {
+		return b_no;
+	}
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+	public String getB_id() {
+		return b_id;
+	}
+	public void setB_id(String b_id) {
+		this.b_id = b_id;
+	}
+	public String getB_title() {
+		return b_title;
+	}
+	public void setB_title(String b_title) {
+		this.b_title = b_title;
+	}
+	public String getB_content() {
+		return b_content;
+	}
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
+	}
+	public int getB_hit() {
+		return b_hit;
+	}
+	public void setB_hit(int b_hit) {
+		this.b_hit = b_hit;
+	}
+	public int getB_renum() {
+		return b_renum;
+	}
+	public void setB_renum(int b_renum) {
+		this.b_renum = b_renum;
+	}
+	public Date getB_date() {
+		return b_date;
+	}
+	public void setB_date(Date b_date) {
+		this.b_date = b_date;
+	}
+	public String getB_delete() {
+		return b_delete;
+	}
+	public void setB_delete(String b_delete) {
+		this.b_delete = b_delete;
+	}
+	
+	
+}
---
+++ TeamProject/src/com/board/dao/QuestionDTO.java
@@ -0,0 +1,60 @@
+package com.board.dao;
+import java.util.*;
+/*
+--1:1문의
+CREATE TABLE question(
+  q_No NUMBER PRIMARY KEY NOT NULL, --문의번호(pk)
+  q_Title VARCHAR2(30) NOT NULL, -- 제목
+  q_Content VARCHAR2(200) NOT NULL, --내용
+  q_Date DATE NOT NULL, --등록일
+  q_Phone VARCHAR2(30), --휴대전화
+  q_Email VARCHAR2(30) NOT NULL, --이메일
+  q_Id VARCHAR2(20) NOT NULL, -- 작성자(fk)
+  q_qtNo NUMBER REFERENCES question_type(qt_No) -- 문의유형 번호(fk)
+);
+ */
+public class QuestionDTO {
+	private int q_no;
+	private String q_title;
+	private String q_content;
+	private Date q_date;
+	private String q_phone;
+	private String q_email;
+	public int getQ_no() {
+		return q_no;
+	}
+	public void setQ_no(int q_no) {
+		this.q_no = q_no;
+	}
+	public String getQ_title() {
+		return q_title;
+	}
+	public void setQ_title(String q_title) {
+		this.q_title = q_title;
+	}
+	public String getQ_content() {
+		return q_content;
+	}
+	public void setQ_content(String q_content) {
+		this.q_content = q_content;
+	}
+	public Date getQ_date() {
+		return q_date;
+	}
+	public void setQ_date(Date q_date) {
+		this.q_date = q_date;
+	}
+	public String getQ_phone() {
+		return q_phone;
+	}
+	public void setQ_phone(String q_phone) {
+		this.q_phone = q_phone;
+	}
+	public String getQ_email() {
+		return q_email;
+	}
+	public void setQ_email(String q_email) {
+		this.q_email = q_email;
+	}
+	
+}
